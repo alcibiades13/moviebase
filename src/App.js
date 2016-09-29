1353251a2b5ad1c7c7f1fad244135ade8aef98ce
alcibiades13 <branislav.teofilovic@gmail.com>
2016-09-29 12:32:47 +0200

    renamed MovieSearch component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import SearchForm from './components/SearchForm';
+import MovieSearch from './components/MovieSearch';
 import './App.css';
 
 class App extends Component {
@@ -10,7 +10,7 @@ class App extends Component {
           <h2>Welcome to Moviebase</h2>
         </div>
         <div className="container">
-          <SearchForm />
+          <MovieSearch />
         </div>
       </div>
     );
